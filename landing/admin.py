2de2a44267c1d7457f3dc7949beea693badf8eb6
rcbxd <rbcxdd@gmail.com>
2020-07-24 23:09:51 -0500

    registered the models

--- landing/admin.py
+++ landing/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Category, Skill, SubSkill
+from .models import Category, SubCategory, Skill, SubSkill, Master
 
 admin.site.register(Category)
 admin.site.register(Skill)
 admin.site.register(SubSkill)
+admin.site.register(Master)
+admin.site.register(SubCategory)
