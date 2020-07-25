2f8acbd62f31365c8d5e97712cd9163b984bbf85
rcbxd <rbcxdd@gmail.com>
2020-07-24 23:09:19 -0500

    index page animations

---
+++ landing/static/landing/script.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    if (localStorage.getItem("didLoad") === null) {
+        Array.from(document.getElementsByClassName('nav')[0].getElementsByTagName('a')).forEach(el => {
+            el.style.animation = 'pageLoadLinks 0.3s';
+        });
+        Array.from(document.getElementsByClassName('contacts')[0].getElementsByTagName('a')).forEach(el => {
+            el.style.animation = 'pageLoadLinks 0.3s';
+        });
+        Array.from(document.getElementsByClassName('title-medium')).forEach(el => {
+            el.style.animation = 'pageLoadText 0.3s';
+        })
+    }
+    localStorage.setItem("didLoad", true);
+}
