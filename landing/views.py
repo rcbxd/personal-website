1162c6a6902876d1ad5e2b9327e1cd1b6c556c98
rcbxd <rbcxdd@gmail.com>
2020-07-24 23:09:57 -0500

    wrote the basic views

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Master, Category, SubCategory, Skill, SubSkill
 
 
 def index(request):
-    return HttpResponse("Hello World! This is my website")
+    master = get_object_or_404(Master, pk=1)
+    return render(request, "landing/index.html", locals())
+
+
+def about(request):
+    return render(request, "landing/about.html")
+
+
+def skills(request):
+    categories = Category.objects.all()
+    subcategories = SubCategory.objects.all()
+    skills = Skill.objects.all()
+    subskills = SubSkill.objects.all()
+    return render(request, "landing/skills.html", locals())
+
+
+def projects(request):
+    return render(request, "landing/projects.html")
