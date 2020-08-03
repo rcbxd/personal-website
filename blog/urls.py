e30b0434bf199c6e542071a56341a6ab80a0c6fd
rcbxd <rbcxdd@gmail.com>
2020-08-02 22:14:08 -0500

    added a post view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.blog, name='blog-index')
+    path('', views.blog, name='blog-index'),
+    path('<int:post_id>/', views.blog_post, name='blog-post'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import BlogPost
+#from .models import BlogImage
 
 
 def blog(request):
-    return HttpResponse("Hello Blog")
+    # get all of the posts
+    return render(request, "blog/index.html", locals())
+
+
+def blog_post(request, post_id):
+    # get the post details
+    post = get_object_or_404(BlogPost, pk=post_id)
+    # get the post images
+    return render(request, "blog/blog_post.html", locals())
