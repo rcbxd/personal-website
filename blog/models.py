8f02b2f3aee3f34d99ac786378a2f209ccade795
rcbxd <rbcxdd@gmail.com>
2020-08-02 22:14:00 -0500

    added a blog post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField(max_length=1000)
+    body = models.TextField()
