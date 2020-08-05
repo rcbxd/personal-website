e4147bae6ba6c9dbfc8a9e27e29177a8113275ac
rcbxd <rbcxdd@gmail.com>
2020-08-05 11:29:35 -0500

    image render views

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import BlogPost
+from .models import BlogPost, PostImage
 #from .models import BlogImage
 
 
 def blog(request):
     # get all of the posts
+    posts = BlogPost.objects.all()
     return render(request, "blog/index.html", locals())
 
 
 def blog_post(request, post_id):
-    # get the post details
     post = get_object_or_404(BlogPost, pk=post_id)
-    # get the post images
+    post_images = PostImage.objects.filter(post=post)
+    print(post_images)
     return render(request, "blog/blog_post.html", locals())
