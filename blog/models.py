c4c67418450b5a81db775db0f7c6cc0a00307e6d
rcbxd <rbcxdd@gmail.com>
2020-08-05 11:29:28 -0500

    added and registered the image model

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,15 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField(max_length=1000)
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class PostImage(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField()
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
